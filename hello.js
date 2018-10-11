a24cd17dbe3533efce568118531434d240eeedd9
jplawrence3 <43733650+jplawrence3@users.noreply.github.com>
2018-10-11 13:46:16 -0400

    181011 upload

---
+++ hello.js
@@ -0,0 +1,51 @@
+// $(document).ready(function(){
+
+// 	//tell the javascript what the quotes are 
+// 	var quote1 = "“The way to right wrongs is to turn the light of truth upon them.”";
+// 	var	quote2 = "“The people must know before they can act, and there is no educator to compare with the press.”";
+
+// 	// console.log(quote2);
+
+// 	//Figure out which quote is being displayed
+
+// 	// console.log(quoteOnDisplay);
+
+// 	//If somebody clicks on an image the new quote is swapped
+
+// 	$("img").click(function(){
+// 			// console.log("I clicked on the image.")
+// 			//if the quote on display is quote1, then we want to fade out h1 and we want to swap in the text for quote2.
+// 			var quoteOnDisplay = $("h1").text();
+
+// 			$("h1").fadeOut(0);
+// 			if (quoteOnDisplay == quote1) {
+// 				$("h1").text(quote2);	
+// 			} else {
+// 				$("h1").text(quote1);
+// 			}
+// 			$("h1").fadeIn();			
+
+
+// 			// if (quoteOnDisplay == quote1) {
+// 			// 	$("h1").fadeOut(0);
+// 			// 	$("h1").text(quote2);
+// 			// 	$("h1").fadeIn();	
+// 			// } else {
+// 			// 	$("h1").fadeOut(0);
+// 			// 	$("h1").text(quote1);
+// 			// 	$("h1").fadeIn();		
+// 			// }
+
+
+
+// 		});
+
+
+
+// 	   // jQuery methods go here...
+// 		// $("p").fadeOut(5000);
+// 	 //    $("button").click(function(){
+// 	 //        $("#test").hide();
+// 	 //    });
+
+// });
